8051734125984fde5e29050de4f291d80990bac6
Brad Jones <brad@jones.name>
2018-03-30 14:39:35 -0600

    Initial commit

---
+++ src/CredentialsProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BradJonesLLC\DrupalAwsCredentialsProvider;
+
+use Aws\Credentials\Credentials;
+use Aws\Exception\CredentialsException;
+use GuzzleHttp\Promise;
+use GuzzleHttp\Promise\RejectedPromise;
+
+class CredentialsProvider {
+
+  /**
+   * Return a credential provider.
+   *
+   * @see https://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html#creating-a-custom-provider
+   * @return \Closure
+   */
+  public static function fromKey(string $keyName = 'aws') {
+    return function () use ($keyName) {
+      // This is returned as a function so we can't use standard
+      // dependency injection.
+      /** @var \Drupal\key\KeyRepositoryInterface $keyRepository */
+      $keyRepository = \Drupal::service('key.repository');
+      $config = $keyRepository->getKey($keyName)->getKeyValue();
+      if ($config['key'] && $config['secret']) {
+        return Promise\promise_for(
+          new Credentials($config['key'], $config['secret'])
+        );
+      }
+
+      $msg = 'Could not load AWS credentials from Drupal key.';
+      return new RejectedPromise(new CredentialsException($msg));
+    };
+  }
+
+}
